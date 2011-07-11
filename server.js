d62568f8c4b8fc34248d1c9c95132a1bfb156791
Christian Bäuerlein <fabrik42@gmail.com>
2011-07-11 21:15:54 +0200

    added redis for persistence as heroku often restarts the app

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ var port = process.env.PORT || 3000,
     express = require('express'),
     _ = require('underscore'),
     app = express.createServer(),
+    redis = require("redis"),
 
     Car = require(__dirname+'/static/js/cars').Car,
     CarCollection = require(__dirname+'/static/js/cars').CarCollection,
     Pusher = require(__dirname+'/pusher');
+
+// INITIALIZE REDIS
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  redis = redis.createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  redis = redis.createClient();
+}
 
 // CONSTANTS
 var ALLOWED_EMAILS = process.env.FLINC_MINI_MAP_EMAILS.split('|');
@@ -13,15 +23,20 @@ var NUM_CARS = 2;
 
 // CARS
 var cars = new CarCollection();
-for(var i = 1; i <= NUM_CARS; i++) {
-  cars.add( new Car({
-    name:   'nobody ' +  i,
-    id:     i,
-    lat:    48 + i,
-    lng:    8,
-    icon:   'img/mini.png'
-  }));
-}
+
+redis.get("flinc:mini:map:cars", function (err, reply) {
+  cars.reset(JSON.parse(reply));
+
+  for(var i = cars.size(); i < NUM_CARS; i++) {
+    cars.add( new Car({
+      name:   'nobody ' +  i,
+      id:     i,
+      lat:    48 + i,
+      lng:    8,
+      icon:   'img/mini.png'
+    }));
+  }
+});
 
 // PUSHER
 pusher = new Pusher({
@@ -32,16 +47,17 @@ pusher = new Pusher({
 
 // EXPRESS
 app.configure(function(){
-    app.use(express.bodyParser());
-    app.set('view engine', 'jade');
-    app.set('views', __dirname + '/views');
-    app.use(express.static(__dirname + '/static'));
+  app.use(express.bodyParser());
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
+  app.use(express.static(__dirname + '/static'));
 });
 
 app.get('/', function(req, res){
   res.render('index', { layout: false, locals: { cars: JSON.stringify(cars), pusherKey: pusher.options.appKey } });
 });
 
+// support for facebook canvas post...
 app.post('/', function(req, res){
   res.render('index', { layout: false, locals: { cars: JSON.stringify(cars), pusherKey: pusher.options.appKey } });
 });
@@ -56,6 +72,7 @@ app.put('/cars/:id/via/:email', function(req, res){
   }
 
   cars.get(req.params.id).set(req.body).email(req.params.email);
+  redis.set("flinc:mini:map:cars", JSON.stringify(cars));
   pusher.channel('sync').trigger('cars:update', cars);
 });
 
